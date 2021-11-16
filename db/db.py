8047287df0cb13a3ff4367c04e36f0f3438dc005
Vidya Gopalakrishna <vidya.gopalakrishna@nyu.edu>
2021-11-16 17:44:25 -0500

    Update db.py
    created a fetch cuser function. - Ally, Vidya

--- db/db.py
+++ db/db.py
@@ -10,3 +10,11 @@ def fetch_pets():
     A function to return all pets in the data store.
     """
     return {"tigers": 2, "lions": 3, "zebras": 1}
+
+
+def fetch_cusers():
+    '''
+    A function to return all cusers in the data store.
+    '''
+    return {"Sara": ["woman", 25, ("clubbing", "brunch"), "NYC"] , "John":["man",21, ("bars", "sports"), "NYC"], "Jane":["woman", 32, ("art", "sports"), "NYC"]}
+
