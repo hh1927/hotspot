19d52679ad892dd4bd04fc3043ca92dd0c0dc673
Alisha Bhatia <67240116+alisha-bhatia@users.noreply.github.com>
2021-11-16 20:09:13 -0500

    Update db.py

--- db/db.py
+++ db/db.py
@@ -12,22 +12,23 @@ def fetch_pets():
     return {"tigers": 2, "lions": 3, "zebras": 1}
 
 
-def create_cuser(name, demographic, age, categories, location):
-    """
-    A function that will save a user to the data base
-    """
-    user = {"name": name,
-            "age": age,
-            "demographic": demographic,
-            "Interest Categories": categories,
-            "location": location}
-    create = json.dumps(user)
-    print(create)
+#def create_cuser(name, demographic, age, categories, location):
+#    """
+#    A function that will save a user to the data base
+#    """
+#    user = {"name": name,
+#            "age": age,
+#            "demographic": demographic,
+#            "Interest Categories": categories,
+#            "location": location}
+#    create = json.dumps(user)
+#    print(create)
 
 
 def fetch_cusers():
     '''
     A function to return all cusers in the data store.
     '''
-    return {"Sara": ["woman", 25, ("clubbing", "brunch"), "NYC"], "John": ["man", 21, ("bars", "sports"), "NYC"], 
+    return {"Sara": ["woman", 25, ("clubbing", "brunch"), "NYC"],
+            "John": ["man", 21, ("bars", "sports"), "NYC"],
             "Jane": ["woman", 32, ("art", "sports"), "NYC"]}
