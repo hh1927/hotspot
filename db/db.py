4f8185fe4b7d26caceb868d9f3a2468f61785da2
Alisha Bhatia <67240116+alisha-bhatia@users.noreply.github.com>
2021-11-16 19:53:15 -0500

    Update db.py
    added create_cuser to db file

--- db/db.py
+++ db/db.py
@@ -12,6 +12,19 @@ def fetch_pets():
     return {"tigers": 2, "lions": 3, "zebras": 1}
 
 
+def create_cuser(name, demographic, age, categories, location):
+    """
+    A function that will save a user to the data base
+    """
+    user = {"name": name,
+            "age": age,
+            "demographic": demographic,
+            "Interest Categories": categories,
+            "location": location}
+    create = json.dumps(user)
+    print(create)
+
+
 def fetch_cusers():
     '''
     A function to return all cusers in the data store.
