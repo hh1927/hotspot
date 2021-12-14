3aa9079c6bf01eb4e07cc05f72338f55feccc57a
hh1927 <47250637+hh1927@users.noreply.github.com>
2021-12-14 11:44:23 -0500

    Update db.py

--- db/db.py
+++ db/db.py
@@ -1,8 +1,7 @@
-"""
-This file will manage interactions with our data store.
-At first, it will just contain stubs that return fake data.
-Gradually, we will fill in actual calls to our datastore.
-"""
+
+# This file will manage interactions with our data store.
+# At first, it will just contain stubs that return fake data.
+# Gradually, we will fill in actual calls to our datastore.
 
 
 def fetch_pets():
