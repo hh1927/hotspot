65ee79090626e571f5475286ae9a63cb4e5bd6e4
hh1927 <47250637+hh1927@users.noreply.github.com>
2022-01-05 14:35:04 -0500

    Update endpoints.py
    deleted all whites

--- API/endpoints.py
+++ API/endpoints.py
@@ -60,7 +60,7 @@ class Buser(Resource):
         This method returns all busers.
         """
         return db.fetch_busers()
-   
+
     @api.response(HTTPStatus.OK, 'Success')
     @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
     @api.response(HTTPStatus.NOT_ACCEPTABLE, 'A duplicate key')
@@ -103,7 +103,7 @@ class Inv_Response(Resource):
         returns the response for the Invite
         """
         return db.fetch_invRes()
-   
+
     @api.response(HTTPStatus.OK, 'Success')
     @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
     @api.response(HTTPStatus.NOT_ACCEPTABLE, 'A duplicate key')
