4fae53714c88a0ac27a10ddbc0ca6e34b8a74658
Vidya Gopalakrishna <vidya.gopalakrishna@nyu.edu>
2022-01-09 19:47:21 +0530

    Update endpoints.py
    corrected ALL endpoints

--- API/endpoints.py
+++ API/endpoints.py
@@ -3,38 +3,37 @@ This is the file containing all of the endpoints for our flask app.
 The endpoint called `endpoints` will return all available endpoints.
 """
 from http import HTTPStatus
-# import json
 from flask import Flask, request, jsonify
 from flask_restx import Resource, Api
-# import werkzeug.exceptions as wz
+import werkzeug.exceptions as wz
 
 import db.db as db
-# import werkzeug.exceptions as wz
 
-from flask.json import JSONEncoder
+#from flask.json import JSONEncoder
 
-from bson import json_util
+#from bson import json_util
 
 # define a cu
 # stom encoder point to the json_util provided by pymongo
 # (or its dependency bson)
 
 
-class CustomJSONEncoder(JSONEncoder):
-    def default(self, obj): return json_util.default(obj)
+#class CustomJSONEncoder(JSONEncoder):
+#    def default(self, obj): return json_util.default(obj)
 
 
 app = Flask(__name__)
 api = Api(app)
-app.json_encoder = CustomJSONEncoder
+#app.json_encoder = CustomJSONEncoder
 
-
+#corrected
 @api.route('/endpoints')
 class Endpoints(Resource):
     """
     This class will serve as live, fetchable documentation of what endpoints
     are available in the system.
     """
+    @api.response(HTTPStatus.OK, 'Success')
     def get(self):
         """
         The `get()` method will return a list of available endpoints.
@@ -42,27 +41,12 @@ class Endpoints(Resource):
         endpoints = sorted(rule.rule for rule in api.app.url_map.iter_rules())
         return {"Available endpoints": endpoints}
 
-
-'''
-@api.route('/pets') #Example of an Endpoint
-class Pets(Resource):
-    """
-    This class supports fetching a list of all pets.
-    """
-    def get(self):
-        """
-        This method returns all pets.
-        """
-        return db.fetch_pets()
-'''
-
-
+#corrected
 @api.route('/cusers/create/<username>')
 class CreateCuser(Resource):
     """
     This class supports adding Customer users.
     """
-
     @api.response(HTTPStatus.OK, 'Success')
     @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
     @api.response(HTTPStatus.NOT_ACCEPTABLE, 'A duplicate key')
@@ -70,24 +54,38 @@ class CreateCuser(Resource):
         """
         This method creates a new Customer User.
         """
-        json_data = request.get_json(force=True)
+        ret = db.add_cuser(username)
+        if ret == db.NOT_FOUND:
+            raise (wz.NotFound("User db could not be found.")
+        elif ret == db.DUPLICATE:
+            raise (wz.NotAcceptable(f"user {username} already exists."))
+        else:
+            raise f"{username} added."
+
+        '''json_data = request.get_json(force=True)
         json_data['name'] = username
         db.add_cuser(json_data)
-        return f"{username} added."
+        return f"{username} added."''''
 
-
+#corrected
 @api.route('/cusers/all')
-class GetCuser(Resource):
+class ListCuser(Resource):
     """
-    This class supports fetching a list of all consumer users
+    This class returns a list of all consumer users
     """
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
     def get(self):
         """
         This method returns all customer users.
         """
-        return db.fetch_cusers()
+        allCusers = db.fetch_cusers()
+        if allCusers is None:
+            raise (wz.NotFound("user couldnt be found."))
+        else:
+            return allCusers
 
-
+#corrected
 @api.route('/busers/create/<username>')
 class Buser(Resource):
     """
@@ -97,16 +95,30 @@ class Buser(Resource):
     def post(self, username):
         """
         This method creates a new Business User.
+        """    
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
+    @api.response(HTTPStatus.NOT_ACCEPTABLE, 'A duplicate key')
+    def post(self, username):
         """
-        json_data = request.get_json(force=True)
+        This method creates a new Business User.
+        """
+        ret = db.add_buser(username)
+        if ret == db.NOT_FOUND:
+            raise (wz.NotFound("User db could not be found.")
+        elif ret == db.DUPLICATE:
+            raise (wz.NotAcceptable(f"user {username} already exists."))
+        else:
+            raise f"{username} added."
+        '''json_data = request.get_json(force=True)
         json_data['name'] = username
         # print(json_data)
         db.add_buser(json_data)
-        return jsonify(json_data)
+        return jsonify(json_data)'''
 
-
+#corrected
 @api.route('/busers/all')
-class GetBuser(Resource):
+class ListBuser(Resource):
     """
     This class supports fetching a list of all business users,
     specifically the users who are hosting events.
@@ -115,51 +127,57 @@ class GetBuser(Resource):
         """
         This method returns all business users.
         """
-        return db.fetch_busers()
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
+    def get(self):
+        """
+        This method returns all business users.
+        """
+        allBusers = db.fetch_busers()
+        if allBusers is None:
+            raise (wz.NotFound("user couldnt be found."))
+        else:
+            return allBusers
 
-
+#corrected
 @api.route('/Inv')
 class Inv(Resource):
     """
     This class supports fetching a list of all invites,
     from the business including time, place and number of people allowed.
     """
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
     def get(self):
         """
         This method returns all invs.
         """
-        return db.fetch_invs()
+        allInvs = db.fetch_invs()
+        if allInvs is None:
+            raise (wz.NotFound("invite couldnt be found."))
+        else:
+            return allInvs
 
-
+#corrected
 @api.route('/Inv_Response')
 class Inv_Response(Resource):
     '''
     This class supports the customer accepting or denying their invite
     from the business
     '''
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
     def get(self):
         '''
         returns invite responses
         '''
-        return db.fetch_invResponse()
-    '''
-    @api.response(HTTPStatus.OK, 'Success')
-    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
-    @api.response(HTTPStatus.NOT_ACCEPTABLE, 'A duplicate key')
+        allInvRes = db.get_inv_response()
+        if allInvRes is None:
+            raise (wz.NotFound("invite couldnt be found."))
+        else:
+            return allInvRes
 
-    def post(self, username):
-        """
-        returns the response for the Invite
-        """
-        ret = db.add_inv_response(username)
-        if ret == db.NOT_FOUND:
-            raise (wz.NotFound("User db not found."))
-        elif ret == db.DUPLICATE:
-            raise (wz.NotAcceptable("User name already exists."))
-        return f"{username} added."
-    '''
-
-
+#corrected
 @api.route('/clientList')
 class ClientList(Resource):
     '''
@@ -167,13 +185,19 @@ class ClientList(Resource):
     Providing a list of clients for that business for specific event,
     including number of total people per party, and time
     '''
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
     def get(self):
         '''
         returns the client list for the business user
         '''
-        return db.fetch_clientList()
+        allClientList = db.fetch_clientList()
+        if allClientList is None:
+            raise (wz.NotFound("Client List couldnt be found."))
+        else:
+            return allClientList
 
-
+#corrected
 @api.route('/clientHist')
 class ClientHist(Resource):
     '''
@@ -181,13 +205,19 @@ class ClientHist(Resource):
     for further analytic purposes
     for businesses
     '''
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
     def get(self):
         '''
         returns the list of prev clients for the business user
         '''
-        return db.fetch_clientHist()
+        allClientHist = db.fetch_clientHist()
+        if allClientHist is None:
+            raise (wz.NotFound("Client is new, without any history."))
+        else:
+            return allClientHist
 
-
+#corrected
 @api.route('/recList')
 class recList(Resource):
     '''
@@ -196,13 +226,19 @@ class recList(Resource):
     as well as suggestions, given other favorites from customers with
     similar demographics
     '''
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
     def get(self):
         '''
         returns the recommendation list for the customer user
         '''
-        return db.fetch_recList()
+        allRecList = db.fetch_recList()
+        if allRecList is None:
+            raise (wz.NotFound("Rec List couldnt be found."))
+        else:
+            return allRecList
 
-
+#corrected
 @api.route('/revHist')
 class revHist(Resource):
     '''
@@ -210,11 +246,17 @@ class revHist(Resource):
     for further analytic purposes
     for businesses
     '''
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
     def get(self):
         '''
         returns all reviews the customer user inputted
         '''
-        return db.fetch_revHist()
+        allRevHist = db.fetch_revHist()
+        if allRevHist is None:
+            raise (wz.NotFound("Review History couldnt be found."))
+        else:
+            return allRevHist
 
 
 @api.route('/buser/interest')
@@ -223,11 +265,17 @@ class clientsTypes(Resource):
     This class supports fetching the types of clients
     a business user is interested in.
     """
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
     def get(self):
         """
-        returns client categories of interest.
+        returns client interests.
         """
-        return db.fetch_clientType()
+        allClientType = db.fetch_clientType()
+        if allClientType is None:
+            raise (wz.NotFound("Rec List couldnt be found."))
+        else:
+            return allClientType
 
 
 @api.route('/buser/promos')
@@ -235,8 +283,56 @@ class promos(Resource):
     """
     This class supports fetching that week's promos
     """
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')  
     def get(self):
         """
         returns current promos.
         """
-        return db.fetch_promos()
+        allPromos = db.fetch_promos()
+        if allPromos is None:
+            raise (wz.NotFound("Buser has no available promos."))
+        else:
+            return allPromos
+
+
+@api.route('/cusers/delete/<username>')
+class DeleteCuser(Resource):
+    """
+    This class enables deleting a cuser.
+    While 'Forbidden` is a possible return value, we have not yet implemented
+    a user privileges section, so it isn't used yet.
+    """
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
+    @api.response(HTTPStatus.FORBIDDEN, 'A user can only delete themselves.')
+    def post(self, username):
+        """
+        This method deletes a user from the user db.
+        """
+        ret = db.del_user(username)
+        if ret == db.NOT_FOUND:
+            raise (wz.NotFound(f"Cuser {username} not found."))
+        else:
+            return f"{username} deleted."
+ 
+
+@api.route('/busers/delete/<username>')
+class DeleteBuser(Resource):
+    """
+    This class enables deleting a buser.
+    While 'Forbidden` is a possible return value, we have not yet implemented
+    a user privileges section, so it isn't used yet.
+    """
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
+    @api.response(HTTPStatus.FORBIDDEN, 'A user can only delete themselves.')
+    def post(self, username):
+        """
+        This method deletes a buser from the user db.
+        """
+        ret = db.del_user(username)
+        if ret == db.NOT_FOUND:
+            raise (wz.NotFound(f"buser {username} not found."))
+        else:
+            return f"{username} deleted."
