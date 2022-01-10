1328d3f534a9c503c5197300ce48b9c571e3c64b
Alisha Bhatia <67240116+alisha-bhatia@users.noreply.github.com>
2022-01-09 23:35:54 -0500

    revert dbconnect

--- db/db_connect.py
+++ db/db_connect.py
@@ -4,17 +4,24 @@ This file contains some common MongoDB code.
 import os
 import json
 import pymongo as pm
+from pymongo.server_api import ServerApi
 import bson.json_util as bsutil
-from   pymongo.server_api import ServerApi
+
 
 # all of these will eventually be put in the env:
-user_nm = "ab8541@nyu.edu"
+user_nm = "ab8541"
 cloud_db = "serverlessinstance0.irvgp.mongodb.net"
-#cloud_svc = "serverlessinstance0.mvrqy.mongodb.net"
-passwd = os.environ.get("MONGO_PASSWD", 'test1234$')
+passwd = os.environ.get("MONGO_PASSWD", "")
 cloud_mdb = "mongodb+srv"
 db_params = "retryWrites=true&w=majority"
-db_nm = "hotspot-mongo"
+
+db_nm = "hotspotDB"
+if int(os.environ.get("TEST_MODE", "")) == 1:
+    dn_nm = "test_hotspotDB"
+
+
+REMOTE = "0"
+LOCAL = "1"
 
 client = None
 
@@ -26,31 +33,20 @@ def get_client():
     Also set global client variable.
     """
     global client
-    if os.environ.get("LOCAL_MONGO", False):
+    if os.environ.get("LOCAL_MONGO", REMOTE) == LOCAL:
+        print("Connecting to Mongo locally.")
         client = pm.MongoClient()
     else:
-        server_api=ServerApi('1')
-        client = pm.MongoClient \
-        (   \
-            f"mongodb+srv://{user_nm}:{passwd}@"    \
-            + f"{cloud_db}/{db_nm}?"    \
-            + "retryWrites=true&w=majority",    \
-            server_api=server_api, tls=True,    \
-            tlsAllowInvalidCertificates=True
+        print("Connecting to Mongo remotely.")
+        client = pm.MongoClient(
+            f"mongodb+srv://{user_nm}:{passwd}.@" + f"/{cloud_mdb}?{db_nm}?",
+            +"retryWrites=true&w=majority",
+            server_api=ServerApi("1"),
+            tls=True,
+            tlsAllowInvalidCertificates=True,
         )
     return client
 
-
-def fetch_all(collect_nm, key_nm):
-    all_docs = []
-    for doc in client[db_nm][collect_nm].find():
-        # print(doc)
-        all_docs.append(json.loads(bsutil.dumps(doc)))
-    return all_docs
-
-
-def insert_doc(collect_nm, doc):
-    client[db_nm][collect_nm].insert_one(doc)
 
 def fetch_one(collect_nm, filters={}):
     """
@@ -61,6 +57,18 @@ def fetch_one(collect_nm, filters={}):
 
 def del_one(collect_nm, filters={}):
     """
-    Fetch one record that meets filters.
+    Delete one record that meets filters.
     """
     return client[db_nm][collect_nm].delete_one(filters)
+
+
+def fetch_all(collect_nm, key_nm):
+    all_docs = {}
+    for doc in client[db_nm][collect_nm].find():
+        print(doc)
+        all_docs[doc[key_nm]] = json.loads(bsutil.dumps(doc))
+    return all_docs
+
+
+def insert_doc(collect_nm, doc):
+    client[db_nm][collect_nm].insert_one(doc)
