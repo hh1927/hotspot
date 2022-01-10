617955c7d19ba83abd6a22c0032c67ccb9f620fb
allyhu6 <47250792+allyhu6@users.noreply.github.com>
2022-01-10 00:42:01 -0500

    Update mongo.sh

--- db/mongo.sh
+++ db/mongo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mongosh "mongodb+srv://serverlessinstance0.irvgp.mongodb.net/hotspotDB" --apiVersion 1 --username ab8541@nyu.edu
+mongosh "mongodb+srv://serverlessinstance0.irvgp.mongodb.net/hotspotDB" --apiVersion 1 --username hotspot
