79dcab365c369a50c5e8d05e3f16ba33b4e0b2f8
hh1927 <47250637+hh1927@users.noreply.github.com>
2022-01-10 03:19:25 -0500

    Update mongo_port.sh

--- db/mongo_port.sh
+++ db/mongo_port.sh
@@ -2,7 +2,12 @@
 
 export passwd=$MONGO_PASSWD
 export db="myFirstDatabase"
-export collect="Cusers"
+export collect="Cusers" #maybe cuser_nm
 export key="cuserName"
+
+python3 mongo_port.py $db $collect $key $passwd
+
+export collect="Busers"
+export key="busername"
 
 python3 mongo_port.py $db $collect $key $passwd
