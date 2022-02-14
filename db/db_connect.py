91c3bbbbc78effab9cc80c0d688213f00d672915
Vidya Gopalakrishna <vidya.gopalakrishna@nyu.edu>
2022-02-14 15:33:12 -0500

    Update db_connect.py

--- db/db_connect.py
+++ db/db_connect.py
@@ -40,7 +40,7 @@ def get_client():
     else:
         print("Connecting to Mongo remotely.")
         client = pm.MongoClient(
-            f"mongodb+srv://{user_nm}:{passwd}.@" + f"/{cloud_svc}?{db_nm}?"
+            f"mongodb+srv://{user_nm}:{passwd}@" + f"/{cloud_svc}?{db_nm}?"
             + "retryWrites=true&w=majority",
             server_api=ServerApi("1"),
             tls=True,
