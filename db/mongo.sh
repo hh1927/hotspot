41da541e694317f770c4f337874dc2b67e6d3d74
Vidya Gopalakrishna <vidya.gopalakrishna@nyu.edu>
2022-02-14 19:11:56 -0500

    Update mongo.sh

--- db/mongo.sh
+++ db/mongo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mongosh "mongodb+srv://hotspot:GdfqZFngsJV47rtk@cluster0.q05tp.mongodb.net/hotspot?retryWrites=true&w=majority" --apiVersion 1 --username hotspot
+mongosh "mongodb+srv://hotspot:GdfqZFngsJV47rtk@cluster0.q05tp.mongodb.net/hotspot?retryWrites=true&w=majority" --username hotspot
