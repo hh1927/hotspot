ec80b37b62da336135d0aeb9d7f7b66925d1ddd4
Vidya Gopalakrishna <vidya.gopalakrishna@nyu.edu>
2022-02-23 17:49:41 -0500

    Update endpoints.py
    pair programming - Vidya and Indra

--- API/endpoints.py
+++ API/endpoints.py
@@ -24,7 +24,14 @@ api = Api(app)
 # app.json_encoder = CustomJSONEncoder
 
 
-# corrected
+#PATCH
+##bQuota
+##cDaily
+
+#POST
+##eventInfo
+
+# CHECK
 @api.route("/endpoints")
 class Endpoints(Resource):
     """
@@ -41,9 +48,9 @@ class Endpoints(Resource):
         return {"Available endpoints": endpoints}
 
 
-# corrected
+# CHECK
 @api.route("/cusers/create/<username>")
-class CreateCuser(Resource):
+class cUser(Resource):
     """
     This class supports adding Customer users.
     """
@@ -68,10 +75,10 @@ class CreateCuser(Resource):
         # return f"{username} added."
 
 
-# corrected
+# CHECK
 # updated API route
 @api.route("/cList")
-class ListCuser(Resource):
+class cList(Resource):
     # added additional parameters related to consumer
     """
     parameters then used to select consumers
@@ -91,9 +98,9 @@ class ListCuser(Resource):
             return allCusers
 
 
-# corrected
+# CHECK
 @api.route("/busers/create/<username>")
-class Buser(Resource):
+class bUser(Resource):
     """
     This class supports business users,
     specifically the users who are hosting events.
@@ -119,10 +126,10 @@ class Buser(Resource):
         # return jsonify(json_data)
 
 
-# corrected
+# CHECK
 # updated api route
 @api.route("/blist")
-class ListBuser(Resource):
+class bList(Resource):
     # added additional parameters related to business
     # paramaters then used to select businesses
     """
@@ -141,55 +148,13 @@ class ListBuser(Resource):
         else:
             return allBusers
 
-
-# corrected
-@api.route("/Inv")
-class Inv(Resource):
-    """
-    This class supports fetching a list of all invites,
-    from the business including time, place and number of people allowed.
-    """
-    @api.response(HTTPStatus.OK, "Success")
-    @api.response(HTTPStatus.NOT_FOUND, "Not Found")
-    def get(self):
-        """
-        This method returns all invs.
-        """
-        allInvs = db.fetch_invs()
-        if allInvs is None:
-            raise (wz.NotFound("invite couldnt be found."))
-        else:
-            return allInvs
-
-
-# corrected
-@api.route("/Inv_Response")
-class Inv_Response(Resource):
-    """
-    This class supports the customer accepting or denying their invite
-    from the business
-    """
-
-    @api.response(HTTPStatus.OK, "Success")
-    @api.response(HTTPStatus.NOT_FOUND, "Not Found")
-    def get(self):
-        """
-        returns invite responses
-        """
-        allInvRes = db.get_inv_response()
-        if allInvRes is None:
-            raise (wz.NotFound("invite couldnt be found."))
-        else:
-            return allInvRes
-
-
-# corrected
+# CHECK
 @api.route("/clientList")
-class ClientList(Resource):
+class cList(Resource):
     """
     This class supports the client List for the Business Users
     Providing a list of clients for that business for specific event,
-    including number of total people per party, and time
+    including number of total people per party (Party Size)
     """
 
     @api.response(HTTPStatus.OK, "Success")
@@ -204,115 +169,8 @@ class ClientList(Resource):
         else:
             return allClientList
 
-
-# corrected
-@api.route("/clientHist")
-class ClientHist(Resource):
-    """
-    Gives list of ALL past clients
-    for further analytic purposes
-    for businesses
-    """
-
-    @api.response(HTTPStatus.OK, "Success")
-    @api.response(HTTPStatus.NOT_FOUND, "Not Found")
-    def get(self):
-        """
-        returns the list of prev clients for the business user
-        """
-        allClientHist = db.fetch_clientHist()
-        if allClientHist is None:
-            raise (wz.NotFound("Client is new, without any history."))
-        else:
-            return allClientHist
-
-
-# corrected
-@api.route("/recList")
-class recList(Resource):
-    """
-    This class supports the recommendation List of businesses
-    for the customers. Providing businesses that they are interested in
-    as well as suggestions, given other favorites from customers with
-    similar demographics
-    """
-
-    @api.response(HTTPStatus.OK, "Success")
-    @api.response(HTTPStatus.NOT_FOUND, "Not Found")
-    def get(self):
-        """
-        returns the recommendation list for the customer user
-        """
-        allRecList = db.fetch_recList()
-        if allRecList is None:
-            raise (wz.NotFound("Rec List couldnt be found."))
-        else:
-            return allRecList
-
-
-# corrected
-@api.route("/revHist")
-class revHist(Resource):
-    """
-    Gives list of ALL past reviews
-    for further analytic purposes
-    for businesses
-    """
-
-    @api.response(HTTPStatus.OK, "Success")
-    @api.response(HTTPStatus.NOT_FOUND, "Not Found")
-    def get(self):
-        """
-        returns all reviews the customer user inputted
-        """
-        allRevHist = db.fetch_revHist()
-        if allRevHist is None:
-            raise (wz.NotFound("Review History couldnt be found."))
-        else:
-            return allRevHist
-
-
-@api.route("/buser/interest")
-class clientsTypes(Resource):
-    """
-    This class supports fetching the types of clients
-    a business user is interested in.
-    """
-
-    @api.response(HTTPStatus.OK, "Success")
-    @api.response(HTTPStatus.NOT_FOUND, "Not Found")
-    def get(self):
-        """
-        returns client interests.
-        """
-        allClientType = db.fetch_clientType()
-        if allClientType is None:
-            raise (wz.NotFound("Rec List couldnt be found."))
-        else:
-            return allClientType
-
-
-@api.route("/buser/promos")
-class promos(Resource):
-    """
-    This class supports fetching that week's promos
-    """
-
-    @api.response(HTTPStatus.OK, "Success")
-    @api.response(HTTPStatus.NOT_FOUND, "Not Found")
-    def get(self):
-        """
-        returns current promos.
-        """
-        allPromos = db.fetch_promos()
-        if allPromos is None:
-            raise (wz.NotFound("Buser has no available promos."))
-        else:
-            return allPromos
-
-
 @api.route("/cusers/delete/<username>")
-class DeleteCuser(Resource):
+class DeletecUser(Resource):
     """
     This class enables deleting a cuser.
     While 'Forbidden` is a possible return value, we have not yet implemented
@@ -334,7 +192,7 @@ class DeleteCuser(Resource):
 
 
 @api.route("/busers/delete/<username>")
-class DeleteBuser(Resource):
+class DeletebUser(Resource):
     """
     This class enables deleting a buser.
     While 'Forbidden` is a possible return value, we have not yet implemented
@@ -374,3 +232,23 @@ class partySize(Resource):
         if ret == db.NOT_FOUND:
             raise (wz.NotFound("User db could not be found."))
         return f"{party} added."
+    
+@api.route("/busers/eventInfo")
+class eventInfo(Resource):
+    """
+    This class supports bUsers inputting their daily event information for the cUsers.
+    """
+
+    @api.response(HTTPStatus.OK, "Success")
+    @api.response(HTTPStatus.NOT_FOUND, "Not Found")
+    @api.response(HTTPStatus.NOT_ACCEPTABLE, "A duplicate key")
+    def post(self, eventName, location, price, hours):
+        """
+        This method creates a new event.
+        """
+        ret = db.add_event(eventName, location, price, hours)
+        if ret == db.NOT_FOUND:
+            raise (wz.NotFound("Event doesnt exist yet. Please try again."))
+        elif ret == db.DUPLICATE:
+            raise (wz.NotAcceptable(f"event {eventName} is coming up soon."))
+        return f"{eventName} is ready for tonight."
