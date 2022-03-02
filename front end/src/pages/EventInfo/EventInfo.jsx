711e946f3b185caa15679db2d7ef752928fbffa5
Hannah Harvard <harvard@nyu.edu>
2022-03-02 13:19:46 -0500

    add EventInfo page - Hannah & Ally

---
+++ front end/src/pages/EventInfo/EventInfo.jsx
@@ -0,0 +1,62 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import {useHistory} from 'react-router-dom';
+
+import './EventInfo.css';
+
+export default function EventInfo(){
+  const history = useHistory();
+
+  function navigateToPage(path) {
+    history.push(path);
+  }
+
+  return (
+    <div className="content">
+      <div> //need ability to add photos
+        <form>
+          <label>
+            Location: <input type="text" /> 
+          </label> //need to establish a drop down to create form for that # of guests
+          <br />
+          <label>
+            Date: <input type="text" />
+          </label>
+          <br />
+          <label>
+            Time: <input type="text" />
+          </label>
+          <br />
+          <label>
+            Max Guests Allowed: <input type="text" />
+          </label>
+          <br />
+          <label>
+            General Description: <input type="text" />
+          </label>
+          <br />
+        </form>
+
+      </div>
+      
+      <button
+        onClick={() => navigateToPage('/homepage')} // must remove event from list of events
+        className="page-button"
+      >
+        Delete
+      </button>
+      <button
+        onClick={() => navigateToPage('/homepage')} //needs to prevent users from updating w/o hitting edit beyond this point
+        className="page-button"
+      >
+        Publish
+      </button>
+      <button
+        onClick={() => navigateToPage('/homepage')} //needs to allow users to update info. MUST UPDATE
+        className="page-button"
+      >
+        Edit
+      </button>
+    </div>
+  );
+};
