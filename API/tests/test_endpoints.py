633b0c3df4bd0073258e51f1ac2b65dabdb76f7c
IndraRatna89 <91338901+IndraRatna89@users.noreply.github.com>
2022-03-24 15:08:57 -0700

    Correcting tabs

--- API/tests/test_endpoints.py
+++ API/tests/test_endpoints.py
@@ -1,23 +1,25 @@
-from unittest import TestCase, skip 
+import db.db as db
+import API.endpoints as ep
+from unittest import TestCase, skip
 from flask_restx import Resource, Api
 import random
-import sys 
-sys.path.insert(0,'/home/runner/work/hotspot/hotspot/API')
-import API.endpoints as ep
-import db.db as db
+import sys
+sys.path.insert(0, '/home/runner/work/hotspot/hotspot/API')
 
 HUGE_NUM = 10000000000000  # any big number will do!
+
 
 def new_entity_name(entity_type):
     int_name = random.randint(0, HUGE_NUM)
     return f"new {entity_type}" + str(int_name)
 
+
 class EndpointTestCase(TestCase):
     def setUp(self):
-         pass
+        pass
 
     def tearDown(self):
-         pass
+        pass
 
     def test_hello(self):
         self.assertTrue(True)
@@ -33,7 +35,7 @@ class EndpointTestCase(TestCase):
         ret = ccu.post(new_cuser)
         cusers = db.get_cusers()
         self.assertIn(new_cuser, cusers)
-          
+
     def test_buser(self):
         """
         See if we can successfully create a new business user.
@@ -44,15 +46,15 @@ class EndpointTestCase(TestCase):
         ret = cbu.post(new_user)
         busers = db.get_busers()
         self.assertIn(new_buser, busers)
-    
-    def test_ClientList1(self): 
+
+    def test_ClientList1(self):
         """
         Post-condition 1: return is a dictionary.
         """
         cl = ep.ClientList(Resource)
         ret = cl.get()
         self.assertIsInstance(ret, dict)
-          
+
     def test_ClientList2(self):
         """
         Post-condition 2: keys to the dict are strings
@@ -70,7 +72,7 @@ class EndpointTestCase(TestCase):
         ret = cl.get()
         for val in ret.values():
             self.assertIsInstance(val, list)
- 
+
     def test_partySize(self):
         """
         See if we can successfully post party size
@@ -84,7 +86,7 @@ class EndpointTestCase(TestCase):
         psize = get_party(username)
         print(f'{psize=}')
         self.assertIn(4, psize)
-	
+
     def test_retrievePartySize(self):
         """
         See if we can successfully retrieve correct values
@@ -102,8 +104,8 @@ class EndpointTestCase(TestCase):
         cUser = ep.DeletecUser(Resource)
         cUser.post(newUser)
         self.assertNotIn(newUser, db.fetch_cusers)
-        
-     def deletebUser(self):
+
+    def deletebUser(self):
         """
         Deleting a bUser
         """
@@ -113,18 +115,18 @@ class EndpointTestCase(TestCase):
         bUser.post(newUser)
         self.assertNotIn(newUser, db.fetch_busers)
 
-     def resetPartySize(self):
-    	"""
-    	Resetting Party Size for the next nights events
-    	"""
-    	newPartySize = new_entitity_name("newUser")
-    	db.add_cuser(newUser)
-    	db.add_party(newUser, 4)
-    	db.reset_party(newUser)
-    	partySize.update(newUser)
-    	self.assertNotIn(newUser, db.fetch_cusers)
-    	
-     def deleteEvent(self):
+    def resetPartySize(self):
+        """
+        Resetting Party Size for the next nights events
+        """
+        newPartySize = new_entitity_name("newUser")
+        db.add_cuser(newUser)
+        db.add_party(newUser, 4)
+        db.reset_party(newUser)
+        partySize.update(newUser)
+        self.assertNotIn(newUser, db.fetch_cusers)
+
+    def deleteEvent(self):
         """
         Deleting a deleteEvent
         """
@@ -132,48 +134,49 @@ class EndpointTestCase(TestCase):
         newevent = ep.del_event(Resource)
         eventInfo.post(newevent)
         self.assertNotIn(newevent, db.fetch_events)
-       
+
     def update_bquota(self):
-    	new_quota = new_entitity_name(8)
+        new_quota = new_entitity_name(8)
         business_name = new_entitity_name("test name")
         bquota_list = ep.bquota(Resource)
-    	# ret = bquota_list.updateOne({name:business_name},{$set:{quota:new_quota}})
-    	# self.assertIn(bquota_list)
+        # ret = bquota_list.updateOne({name:business_name},{$set:{quota:new_quota}})
+        # self.assertIn(bquota_list)
         self.assertTrue(True)
-      
-     def test_retrieve_Bquota(self):
+
+    def test_retrieve_Bquota(self):
         """
         See if we can successfully retrieve correct values
         """
         new_quota = new_entitity_name(5)
         business_name = new_entitity_name("test name, retrieval")
-	    # evInf = bquota_list.updateOne({name:business_name},{$set:{quota:new_quota}})
-	    bquota_updated= ep.fetch_bquota()
+        # evInf = bquota_list.updateOne({name:business_name},{$set:{quota:new_quota}})
+        bquota_updated = ep.fetch_bquota()
         # self.assertIn(evInf, bquota_updated)
         self.assertTrue(True)
 
     def cDaily(self):
-    	new_neighborhood = new_entitity_name("test_neighborhood")
-    	new_interests = new_entity_name("test_interests")
-    	user_name = new_entity_name("test_user")
-            cDaily = ep.cDaily(Resource)
-    	# db.cusers.updateOne({name:user_name},{$set:{neighborhood:new_neighborhood}})  
+        new_neighborhood = new_entitity_name("test_neighborhood")
+        new_interests = new_entity_name("test_interests")
+        user_name = new_entity_name("test_user")
+        cDaily = ep.cDaily(Resource)
+        # db.cusers.updateOne({name:user_name},{$set:{neighborhood:new_neighborhood}})
         # db.cusers.updateOne({name:user_name},{$set:{interests:new_interests}})
-    	self.assertIn(cdaily, db.fetch_cusers)
-	
+        self.assertIn(cdaily, db.fetch_cusers)
+
     def test_eventInfoExists(self):
         """
-        See if we can successfully post event info 
+        See if we can successfully post event info
         """
-        evInf = ep.add_event("Test event", "Test location", "test price", "test hours")
-	    isExists = event_exists("Test event", "Test location")
+        evInf = ep.add_event("Test event", "Test location",
+                             "test price", "test hours")
+        isExists = event_exists("Test event", "Test location")
         self.assertTrue(4, isExists)
-	
-     def test_eventInfo(self):
+
+    def test_eventInfo(self):
         """
         See if we can successfully retrieve correct values
         """
-        evInf = ep.add_event("Test event", "Test location", "test price", "test hours")
-	    allEvents = ep.fetch_events()
+        evInf = ep.add_event("Test event", "Test location",
+                             "test price", "test hours")
+        allEvents = ep.fetch_events()
         self.assertIn(evInf, allEvents)
-	
