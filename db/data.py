c7091408c91abc884d93fda3179cc296c185daac
Vidya Gopalakrishna <vidya.gopalakrishna@nyu.edu>
2022-03-29 12:56:02 -0400

    Update data.py
    whitespace on line 180

--- db/data.py
+++ db/data.py
@@ -175,11 +175,9 @@ def add_party(username, party):
     if cuser_exists(username):
         dbc.insert_doc(CUSERS, {CUSER_NM: username}, {PARTY: party})
         return OK
-    else:
-        return NOT_FOUND
 
 
- def get_party(username):
+def get_party(username):
      """
      Function to return user's party size
      """
