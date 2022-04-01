53e3c8e889b0718990c8549f7cba5606c60e5896
Hannah Harvard <harvard@nyu.edu>
2022-04-01 13:13:18 -0400

    fixed compiling errors to view again

--- front end/src/App.js
+++ front end/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Login from './pages/Login/Login';
-import CreateAcc from './pages/createAcc/CreateAcc';
+import CreateAcc from './pages/CreateAcc/CreateAcc';
 import CHome from './pages/CUSERSIDE/CHOME/CHOME';
 import Users from './pages/Users/Users';
 import RSVP from './pages/CUSERSIDE/RSVP/RSVP';
