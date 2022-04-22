9513d296c239c745413a3119742873dcfda0e8b6
Vidya Gopalakrishna <vidya.gopalakrishna@nyu.edu>
2022-04-22 17:30:02 -0400

    Update Cuserprofile.jsx
    added server requests for Cuser profile

--- front end/src/pages/CUSERSIDE/Cuserprofile/Cuserprofile.jsx
+++ front end/src/pages/CUSERSIDE/Cuserprofile/Cuserprofile.jsx
@@ -5,11 +5,44 @@ import {useHistory} from 'react-router-dom';
 import './Cuserprofile.css';
 
 export default function Cuserprofile(){
+   const [Cuserprofile, setCuserprofile] = useState(undefined);
+   const [error, setError] = useState(undefined);
+
+   const [refresh, setRefresh] = useState(0);
+
+   const [isModalOpen, setIsModalOpen] = useState(false);
+   const [newEvent, setNewEvent] = useState('');
   const history = useHistory();
 
   function navigateToPage(path) {
     history.push(path);
   }
+  
+  useEffect(() => {
+     axios.get('https://teamhotspot.herokuapp.com/cUsers')
+       .then((response) => {
+         if (response.data){
+           setNewEvent(response.data);
+         }
+       })
+       .catch(error => {
+         setError(error);
+         console.log(error);
+       });
+   }, [refresh])
+
+   const handleEventInfo = () => {
+     axios.post(`https://demo-repo23.herokuapp.com/cUsers/${user_name}/${age}/${interests}/${neighborhoods}`)
+       .then(() => {
+         setIsModalOpen(false);
+         setRefresh(refresh + 1);
+       })
+       .catch(error => {
+         setError(error);
+         console.log(error);
+       })
+   }
+
 
   return (
     <div className="content">
