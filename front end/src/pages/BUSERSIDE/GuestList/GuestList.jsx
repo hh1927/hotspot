1d568411f13229acee75b5b8af402ed239a97936
Indra Ratna <indraratna@10-16-106-243.dynapool.wireless.nyu.edu>
2022-05-06 16:02:22 -0400

    adding server calls for guest list page Vidya &Indra

--- front end/src/pages/BUSERSIDE/GuestList/GuestList.jsx
+++ front end/src/pages/BUSERSIDE/GuestList/GuestList.jsx
@@ -1,6 +1,41 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import {useHistory} from 'react-router-dom';
+
 import './GuestList.css';
 
-function GuestList() {
+export default function GuestList() {
+    const history = useHistory();
+    const [newVenueProfile, setVenueProfile] = useState(undefined);
+
+    useEffect(() => {
+        axios.get('https://teamhotspot.herokuapp.com/GuestList/cList')
+        .then((response) => {
+            if (response.data){
+            setUsers(response.data);
+            }
+        })
+        .catch(error => {
+            setError(error);
+            console.log(error);
+        });
+    }, [refresh])
+
+    const handlecList = () => {
+        axios.post(`https://teamhotspot.herokuapp.com/GuestList/cList`)
+        .then(() => {
+            setIsModalOpen(false);
+            setRefresh(refresh + 1);
+        })
+        .catch(error => {
+            setError(error);
+            console.log(error);
+        })
+    }
+
+    function navigateToPage(path) {
+        history.push(path);
+    }
     return (
         <>
             <a href = '/bHome'><h1>HotSpot </h1></a>
@@ -63,6 +98,9 @@ function GuestList() {
             </a>
             <a href = '/venueInfo'>
             <button id= "nextButton"> VIEW VENUE </button>
+            </a>
+            <a href = '/eventInfo'>
+            <button onClick={handleBquota} id= "resButton"> VIEW CUSTOMER LIST </button>
             </a>
             </>
     )
