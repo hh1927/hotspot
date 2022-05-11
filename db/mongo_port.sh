ea17b697602fa2fc3caa3af13245f4fca9676419
Vidya Gopalakrishna <vidya.gopalakrishna@nyu.edu>
2022-05-11 07:42:04 -0400

    Update mongo_port.sh

--- db/mongo_port.sh
+++ db/mongo_port.sh
@@ -4,14 +4,8 @@ export passwd=$MONGO_PASSWD
 export db="hotspot"
 export collect="business"
 export key="business_name"
-
-python3 mongo_port.py $db $collect $key $passwd
-
 export collect="consumer"
 export key="username"
-
-python3 mongo_port.py $db $collect $key $passwd
-
 export collect="events"
 export key="event_name"
 
